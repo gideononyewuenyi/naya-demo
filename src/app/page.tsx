4bbfe5887436c35ca5c80497a194f2f4289dc988
Gideon Onyewuenyi <36352230+gideononyewuenyi@users.noreply.github.com>
2024-07-06 17:53:58 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       axios
         .post("https://nayapp.azurewebsites.net/agent/process", {
           data: input,
-        },
+        }, {
           headers: {
             'Authorization': `Bearer ${process.env.API_KEY}`,
             'Content-Type': 'application/json'
